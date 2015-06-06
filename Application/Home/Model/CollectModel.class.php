bf901287259d8ab9c565abda77bd95bb0e0cb7df
Dicky Wong <huangjw0124@163.com>
2015-06-06 14:47:45 +0800

    添加Collect模型

---
+++ Application/Home/Model/CollectModel.class.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Home\Model;
+
+use Think\Model;
+
+class CollectModel extends Model {
+	public function addCollect($us_nu, $re_nu) {
+		$data ['us_nu'] = $us_nu;
+		$data ['re_nu'] = $re_nu;
+		return $this->add ( $data );
+	}
+	public function deleteCollect($co_nu) {
+		return $this->delete ( $co_nu );
+	}
+	public function getCollect($us_nu, $page = 1) {
+		$condition ['us_nu'] = $us_nu;
+		return $this->where ( $condition )->page ( $page, 10 )->select ();
+	}
+}
